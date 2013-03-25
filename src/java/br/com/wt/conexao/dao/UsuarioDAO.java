b451cbe7604a0ce15bdeaabeeab16ecb95bb49c1
Danilo Oliveira Lima <danilo__oliveira@hotmail.com>
2013-03-24 23:32:35 -0300

    Adicionado metodo o buscaPorId e atualiza em UsuarioDAO

--- src/java/br/com/wt/conexao/dao/UsuarioDAO.java
+++ src/java/br/com/wt/conexao/dao/UsuarioDAO.java
@@ -1,5 +1,6 @@
 package br.com.wt.conexao.dao;
 
+import br.com.wt.entities.Tempo;
 import br.com.wt.entities.Usuario;
 import java.util.HashMap;
 import java.util.Map;
@@ -25,5 +26,21 @@ public class UsuarioDAO {
             return null;
         }
         return usuario;
+    }
+    
+    public Usuario buscaPorId(Integer id){
+        Usuario usuario = dao.buscaPorId(Usuario.class, id);
+        
+        if(usuario == null){
+            return null;
+        }
+        return usuario;
+    }
+    
+    public Boolean atualiza(Usuario t){
+        if(dao.persiste(t, "atualizar") != null){
+            return true;
+        }
+        return false;
     }
 }
