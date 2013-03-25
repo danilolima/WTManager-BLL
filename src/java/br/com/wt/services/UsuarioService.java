e92d6eeb56124cf5bf4e62cfe65cc8c0897b06bc
Danilo Oliveira Lima <danilo__oliveira@hotmail.com>
2013-03-24 23:33:15 -0300

    Adicionado metodo de adicionar Tempo em UsuarioService

--- src/java/br/com/wt/services/UsuarioService.java
+++ src/java/br/com/wt/services/UsuarioService.java
@@ -1,7 +1,11 @@
 package br.com.wt.services;
 
+import br.com.wt.conexao.dao.TempoDAO;
 import br.com.wt.conexao.dao.UsuarioDAO;
+import br.com.wt.entities.Tempo;
 import br.com.wt.entities.Usuario;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -9,15 +13,31 @@ import br.com.wt.entities.Usuario;
  */
 public class UsuarioService {
 
-    private static Usuario usuario;
     private UsuarioDAO usuarioDAO;
+    private TempoDAO tempoDAO;
 
     public UsuarioService() {
         this.usuarioDAO = new UsuarioDAO();
+        this.tempoDAO = new TempoDAO();
     }
 
     public Usuario getUsuario(String chave) {
+        Usuario usuario = null;
         usuario = this.usuarioDAO.buscaPorChave(chave);
         return usuario;
+    }
+    
+    public Boolean adicionaTempo(Tempo tempo){
+        if(tempo != null){
+            tempo.setId(null);
+            try{
+                this.tempoDAO.atualiza(tempo);
+                return true;
+            }catch (Exception e){
+                Logger.getLogger(UsuarioService.class.getName()).log(Level.SEVERE, null, e);
+                return false;
+            }
+        }
+        return false;
     }
 }
